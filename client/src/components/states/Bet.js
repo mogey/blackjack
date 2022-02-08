a7d08e3f97f2bcade8eb65ecdd82914f53fd74a5
Mohsin Qureshi <master.55101@gmail.com>
2022-02-08 16:42:38 -0500

    Bet field now auto focuses, hitting enter on bet field bets the amount

--- client/src/components/states/Bet.js
+++ client/src/components/states/Bet.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { Row, Col, Button, Form } from "react-bootstrap";
-import useSound from 'use-sound';
+import useSound from "use-sound";
 import { bet, deal } from "../../services/blackjack.service";
-import betSfx from '../../audio/bet.mp3';
+import betSfx from "../../audio/bet.mp3";
 
 export default function Bet(props) {
   const { game, refetcher, user } = props; //destructure props so we know what we have
 
   const [betAmount, setBetAmount] = useState();
-  const [playBet] = useSound(betSfx)
+  const [playBet] = useSound(betSfx);
   const rowPadding = {
     margin: "25px auto",
     width: "50%",
@@ -18,7 +18,8 @@ export default function Bet(props) {
     setBetAmount(e.target.value);
   };
 
-  const handleBetClick = () => {
+  const handleBetClick = (event) => {
+    event.preventDefault();
     bet(betAmount, user).then((response) => {
       if (response.status === 200) {
         playBet();
@@ -42,9 +43,10 @@ export default function Bet(props) {
     <React.Fragment>
       <Row style={rowPadding} className="justify-content-center">
         <Col>
-          <Form inline>
+          <Form inline onSubmit={handleBetClick}>
             <Form.Label style={{ margin: "10px" }}>Place your bet</Form.Label>
             <Form.Control
+              autoFocus
               onChange={(e) => {
                 handleBetChange(e);
               }}
@@ -59,10 +61,8 @@ export default function Bet(props) {
         <Row style={rowPadding}>
           <Col>
             <Button
-              onClick={() => {
-                handleBetClick();
-              }}
               style={{ backgroundColor: "#2D6233" }}
+              onClick={handleBetClick}
             >
               Bet
             </Button>
