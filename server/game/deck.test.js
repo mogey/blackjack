beacd538130acccf73afa7af6466b2cad4ca6fe2
Mohsin Qureshi <master.55101@gmail.com>
2022-02-08 16:20:32 -0500

    Reworking backend code for deck class to handle detection of poker hands

---
+++ server/game/deck.test.js
@@ -0,0 +1,24 @@
+import Deck from "./deck.js";
+import Card from "./card.js";
+
+let MyDeck = new Deck();
+
+//Test for royal flush
+
+//Test for straight flush
+
+//Test for four of a kind
+
+//Test for full house case
+
+//test for flush
+
+//test for straight
+
+//test for three of a kind
+
+//test for two pairs
+
+//test for 1 pair
+
+//test for high card
